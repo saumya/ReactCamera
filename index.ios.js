88531047565cd3a9095243f37617400ecd06bbb2
saumya <2saumyaray@gmail.com>
2016-04-04 13:23:59 +0530

    iOS: settings done in XCode

--- index.ios.js
+++ index.ios.js
@@ -8,7 +8,8 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
 } from 'react-native';
 
 class ReactWithProfiles extends Component {
@@ -18,8 +19,14 @@ class ReactWithProfiles extends Component {
         <Text>
           Welcome to React Native and React camera.
         </Text>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <Text style={styles.button}> Camera </Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+  _onPressButton(){
+    alert('hi');
   }
 }
 
@@ -29,6 +36,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#CCCCCC',
     top:22,
   },
+  button:{
+    backgroundColor: '#222222',
+    color: '#FFFFFF',
+    height:50,
+    width: 100,
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 10,
+    marginLeft:20,
+    marginTop:20,
+  }
 });
 
 AppRegistry.registerComponent('ReactWithProfiles', () => ReactWithProfiles);
